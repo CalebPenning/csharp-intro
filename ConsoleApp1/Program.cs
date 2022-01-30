1a5c7d402d04a71929676f8fe019d28fece40f00
Caleb Penning <calebpenning@mail.com>
2022-01-30 13:01:09 -0800

    messing with structs and classes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
+
+            Console.WriteLine("Enter first number: ");
+            string sNum1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second number: ");
+            string sNum2 = Console.ReadLine();
+
+            Int32.TryParse(sNum1, out num1);
+            Int32.TryParse(sNum2, out num2);
+
+            result = num1 + num2;
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}
