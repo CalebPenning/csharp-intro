4dc84301bb8311e5b54fb5b6df4cef58c975a33c
Caleb Penning <calebpenning@mail.com>
2022-01-30 15:23:35 -0800

    Added some stuff

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 namespace HelloWorld
 {
     class Program
-    {
+    {   
+        // Doing some genetic modification
+        static void MultipleLegs(PetStruct petStruct, Pet petClass)
+        {
+            petStruct.Legs = petStruct.Legs * 2;
+            petClass.Legs = petClass.Legs * 2;
+
+            Console.WriteLine($"Internal Method - A {petStruct.Type} has {petStruct.Legs} legs.");
+            Console.WriteLine($"Interal Method - A {petClass.Type} has {petClass.Legs} legs.");
+            Console.ReadLine(); 
+        }
+
         static void Main(string[] args)
         {
             PetStruct dog;
@@ -25,6 +36,8 @@ namespace HelloWorld
             Console.WriteLine($"Hello! I'm {dog.Name} the {dog.Type}! I have {dog.Legs} legs and if you're wondering if I have fur, it's {dog.HasFur}");
             Console.WriteLine($"Hello! I'm {duck.Name} the {duck.Type}! I have {duck.Legs} legs and if you're wondering if I have fur, it's {duck.HasFur}");
             Console.ReadLine();
+
+            MultipleLegs(dog, duck);
         }
     }
 
